0046714cea1781ce571ecaedd9f378a83ca147a1
gutovp1 <paivaav@gmail.com>
2023-09-22 07:56:56 -0300

    Create globalstyles with reset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import StyledComponentsRegistry from '@/lib/registry'
+import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,9 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-        <body className={inter.className}>
-          <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
-        </body>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>
+          <Providers>{children}</Providers>
+        </StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
