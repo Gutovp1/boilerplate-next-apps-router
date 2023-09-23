74d02eda034372eb78afbaa49df4e44392b03e4b
gutovp1 <paivaav@gmail.com>
2023-09-22 23:57:59 -0300

    Create an sb static build with controls

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,20 @@
-import * as S from './styles';
+import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS and Styled Components'
+}) => (
   <S.Wrapper>
-    <h1>React Avançado</h1>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React avançado escrito do lado."
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código."
+    />
   </S.Wrapper>
 )
 
